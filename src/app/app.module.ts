2fa3598eb4273179159245c4ca7aaeb978af7e73
Carlos.Gaffo <carlos.gaffo@live.com>
2024-05-28 17:36:53 -0300

    1212

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-//AngularComponents
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
